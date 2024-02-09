1e7daad19fdacaed172c26bf181586e4641c90e9
alexialang <alexialang2@gmail.com>
2024-02-09 11:34:04 +0100

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoF2ZwScN571qroz68eB90PnTxWNwnVj0",
+  authDomain: "calculette-js.firebaseapp.com",
+  projectId: "calculette-js",
+  storageBucket: "calculette-js.appspot.com",
+  messagingSenderId: "699857473757",
+  appId: "1:699857473757:web:f359154f80ef669b79cb34",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
